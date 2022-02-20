d7abc0dd90db5e2c8101f9d2f687cd4e44965dba
Eugene Low <eugene.ws.low@gmail.com>
2022-02-20 18:38:30 +0800

    Publish to test live

--- wikirace/script.js
+++ wikirace/script.js
@@ -1,5 +1,5 @@
 const loadsite = async () => {
-    const response = await fetch('https://en.wikipedia.org/w/api.php?action=parse&format=json&page=Pet_door&prop=text&formatversion=2&origin=*', {
+    const response = await fetch('https://en.wikipedia.org/w/api.php?action=parse&format=json&page=India&prop=text&disableeditsection=1&redirects=1&formatversion=2&useskin=modern&origin=*', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
