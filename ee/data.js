0114fc3d6e76462c510414e670f6e5b12c456d3d
Eugene Low <eugene.ws.low@gmail.com>
2022-11-25 14:52:36 +0800

    Add in gallery

---
+++ ee/data.js
@@ -0,0 +1,36 @@
+sample = 
+{
+    "intro":"Real one.",
+    "content":[
+        {
+            "image":"FullscreenImageSlider/assets/photo1.jpeg",
+            "title":"Test, Digital",
+            "desc":"Test Link is www.google.com"
+        },
+        {
+            "image":"FullscreenImageSlider/assets/photo2.jpeg",
+            "title":"Test, Digital",
+            "desc":"Link<br/>NewLine"
+        },
+        {
+            "image":"FullscreenImageSlider/assets/photo3.jpeg",
+            "title":"Test, Digital",
+            "desc":null
+        },
+        {
+            "image":"FullscreenImageSlider/assets/photo4.jpeg",
+            "title":"Test, Digital",
+            "desc":null
+        },
+        {
+            "image":"FullscreenImageSlider/assets/photo5.jpeg",
+            "title":"Test, Digital",
+            "desc":null
+        },
+        {
+            "image":"FullscreenImageSlider/assets/photo6.jpeg",
+            "title":"Test, Digital",
+            "desc":null
+        }
+    ] 
+}
