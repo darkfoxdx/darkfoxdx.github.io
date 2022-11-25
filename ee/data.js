ac17ea3680efae453ae006bd219be5b68feb7ef3
Eugene Low <eugene.ws.low@gmail.com>
2022-11-26 00:58:11 +0800

    Add images and json

--- ee/data.js
+++ ee/data.js
@@ -3,33 +3,147 @@ sample =
     "intro":"Real one.",
     "content":[
         {
-            "image":"FullscreenImageSlider/assets/photo1.jpeg",
-            "title":"Test, Digital",
-            "desc":"Test Link is www.google.com"
+            "image":"./images/01.jpg",
+            "title":"Yris Black", 
+            "subtitle":"Colour pencils on paper",
+            "desc":"https://www.instagram.com/yris_black/"
         },
         {
-            "image":"FullscreenImageSlider/assets/photo2.jpeg",
-            "title":"Test, Digital",
-            "desc":"Link<br/>NewLine"
-        },
-        {
-            "image":"FullscreenImageSlider/assets/photo3.jpeg",
-            "title":"Test, Digital",
+            "image":"./images/02.jpg",
+            "title":"Kenn",
+            "subtitle":"Digital",
             "desc":null
         },
         {
-            "image":"FullscreenImageSlider/assets/photo4.jpeg",
-            "title":"Test, Digital",
+            "image":"./images/03.png",
+            "title":"Ayyk92",
+            "subtitle":"Digital",
             "desc":null
         },
         {
-            "image":"FullscreenImageSlider/assets/photo5.jpeg",
-            "title":"Test, Digital",
+            "image":"./images/04.jpg",
+            "title":"Eliot Lee",
+            "subtitle":"Digital",
             "desc":null
         },
         {
-            "image":"FullscreenImageSlider/assets/photo6.jpeg",
-            "title":"Test, Digital",
+            "image":"./images/05.jpg",
+            "title":"Jae Sern",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/06.jpg",
+            "title":"Reimena Yee",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/07.jpg",
+            "title":"Szetoo",
+            "subtitle":"Watercolour on paper",
+            "desc":null
+        },
+        {
+            "image":"./images/08.jpg",
+            "title":"Ng Ze-Ren",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/09.png",
+            "title":"Cntrl",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/10.png",
+            "title":"Narassin",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/11.png",
+            "title":"Fathiah Raihana",
+            "subtitle":"Watercolours on paper",
+            "desc":null
+        },
+        {
+            "image":"./images/12.jpg",
+            "title":"Jia Ming",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/13.jpg",
+            "title":"Shaveena Jane",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/14.jpg",
+            "title":"Panda Yoong",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/15.png",
+            "title":"Kerjacomot",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/16.jpg",
+            "title":"Zi-Lynn",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/17.jpg",
+            "title":"Kaelyn Chan",
+            "subtitle":"Watercolour and gouache on paper",
+            "desc":null
+        },
+        {
+            "image":"./images/18.jpg",
+            "title":"Blueblurrry",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/19.png",
+            "title":"Mae Schulze",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/20.png",
+            "title":"AldoDitto",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/21.png",
+            "title":"Valaryon",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/22.jpg",
+            "title":"Eva H",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/23.jpg",
+            "title":"Ikmal Fauzi",
+            "subtitle":"Digital",
+            "desc":null
+        },
+        {
+            "image":"./images/24.png",
+            "title":"Lazy",
+            "subtitle":"Digital",
             "desc":null
         }
     ] 
