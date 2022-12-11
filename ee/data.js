f94c3f8165682e10e58b02809bd3d27e7c3e2dad
Eugene Low <eugene.ws.low@gmail.com>
2022-12-11 09:33:24 +0800

    Update data

--- ee/data.js
+++ ee/data.js
@@ -1,150 +1,150 @@
 sample = 
 {
-    "intro":"Real one.",
+    "intro":"This project started on Sep 3, 2020. The idea was the support all these wonderful artists inside by getting them to commission an artwork each. These artwork each are meaningful to us in its own way. Thank you to all who have contributed and all of you who are visiting.",
     "content":[
         {
             "image":"./images/01.jpg",
             "title":"Yris Black", 
             "subtitle":"Colour pencils on paper",
-            "desc":"https://www.instagram.com/yris_black/"
+            "desc":"https://www.instagram.com/yris_black"
         },
         {
             "image":"./images/02.jpg",
             "title":"Kenn",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/meanhouseart"
         },
         {
             "image":"./images/03.png",
             "title":"Ayyk92",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://twitter.com/ayyk92"
         },
         {
             "image":"./images/04.jpg",
             "title":"Eliot Lee",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/el_ot"
         },
         {
             "image":"./images/05.jpg",
             "title":"Jae Sern",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/jaesern_art"
         },
         {
             "image":"./images/06.jpg",
             "title":"Reimena Yee",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/reimenayee"
         },
         {
             "image":"./images/07.jpg",
             "title":"Szetoo",
             "subtitle":"Watercolour on paper",
-            "desc":null
+            "desc":"https://www.instagram.com/stickerrific"
         },
         {
             "image":"./images/08.jpg",
             "title":"Ng Ze-Ren",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/zerenng"
         },
         {
             "image":"./images/09.png",
             "title":"Cntrl",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/control_undo"
         },
         {
             "image":"./images/10.png",
             "title":"Narassin",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/captain_narassin"
         },
         {
             "image":"./images/11.png",
             "title":"Fathiah Raihana",
             "subtitle":"Watercolours on paper",
-            "desc":null
+            "desc":"https://www.instagram.com/fathiahraihana"
         },
         {
             "image":"./images/12.jpg",
             "title":"Jia Ming",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/jiamingsuen"
         },
         {
             "image":"./images/13.jpg",
             "title":"Shaveena Jane",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/shavi_jane_art"
         },
         {
             "image":"./images/14.jpg",
             "title":"Panda Yoong",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/pandayoong__"
         },
         {
             "image":"./images/15.png",
             "title":"Kerjacomot",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/kerjacomot"
         },
         {
             "image":"./images/16.jpg",
             "title":"Zi-Lynn",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://zeearts.tumblr.com"
         },
         {
             "image":"./images/17.jpg",
             "title":"Kaelyn Chan",
             "subtitle":"Watercolour and gouache on paper",
-            "desc":null
+            "desc":"My cousin"
         },
         {
             "image":"./images/18.jpg",
             "title":"Blueblurrry",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/blueblurrry"
         },
         {
             "image":"./images/19.png",
             "title":"Mae Schulze",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/maepetals"
         },
         {
             "image":"./images/20.png",
             "title":"AldoDitto",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/aldodittoart"
         },
         {
             "image":"./images/21.png",
             "title":"Valaryon",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/valaryonart"
         },
         {
             "image":"./images/22.jpg",
             "title":"Eva H",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/evasapphire.gaming"
         },
         {
             "image":"./images/23.jpg",
             "title":"Ikmal Fauzi",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://www.instagram.com/ikmalfauzi_art"
         },
         {
             "image":"./images/24.png",
             "title":"Lazy",
             "subtitle":"Digital",
-            "desc":null
+            "desc":"https://twitter.com/LazyArtist__"
         }
     ] 
 }
