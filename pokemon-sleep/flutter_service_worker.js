638838e05ea97a4d7fdb76035ea1ae47d953298f
Eugene Low <eugene.ws.low@gmail.com>
2024-02-18 22:55:32 +0800

    CORS

--- pokemon-sleep/flutter_service_worker.js
+++ pokemon-sleep/flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d04d0cf0c2d5c2f6f27469d9776dfcc4",
-"/": "d04d0cf0c2d5c2f6f27469d9776dfcc4",
-"main.dart.js": "236dd98f2cc59951007a5fbd2b78b3ec",
+"index.html": "c6c19823a1c1dd55d661622da2f0ac6b",
+"/": "c6c19823a1c1dd55d661622da2f0ac6b",
+"main.dart.js": "1b05b9218471586d00c46d6930f06891",
 "manifest.json": "af8083c3232d7358634498c8820fced1",
 "version.json": "f7f9f2918205b5834f3d42a1b1d8630c"};
 // The application shell files that are downloaded before a service worker can
